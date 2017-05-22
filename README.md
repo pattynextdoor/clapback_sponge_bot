25d02dbafff07a9c53955d821caec00d2214ea72
Patrick Tumbucon <ptumb001@ucr.edu>
2017-05-22 02:55:40 -0700

    unfinished

---
+++ README.md
@@ -0,0 +1,9 @@
+# Spongebob Clapback
+
+Visit the user @clapbacksponge at https://twitter.com/clapbacksponge
+
+# How to use this:
+
+In `keys.py`, change all `Redacted` values to the respective keys/tokens of your Twitter application.
+
+Tweepy needs to be installed on your computer.
---
+++ keys.py
@@ -0,0 +1,6 @@
+keys = dict(
+	consumer_key =          'SIXkgQoYWJhUmJ649rK0ZtnKg',
+	consumer_secret =       '72v2ORjF2vdc1ZEwjyhAEMNEmbrPJeueVZpaliGGF229gGKuXE',
+	access_token =          '866555918774710272-VGODflOjhuDIkzcJao7kCak267whSn5',
+	access_token_secret =   'QagjIus83GW0j3vEMWFGSSRexG6suk94u94C5IrLCEMi9'
+)
