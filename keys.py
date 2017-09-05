143f7933a8db9910a84b045d254361cfa93480d9
Patrick Tumbucon <ptumb001@ucr.edu>
2017-09-05 12:55:48 -0700

    Delete keys.py

--- keys.py
+++
@@ -1,6 +0,0 @@
-keys = dict(
-	consumer_key =          'placeholder',
-	consumer_secret =       'placeholder',
-	access_token =          'placeholder-placeholder',
-	access_token_secret =   'placeholder'
-)
