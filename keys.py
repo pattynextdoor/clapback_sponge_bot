c855dfea229eba48c32f8b8476efb94f42cfc6b1
Patrick Tumbucon <ptumb001@ucr.edu>
2017-09-05 12:56:06 -0700

    Create keys.py

---
+++ keys.py
@@ -0,0 +1,6 @@
+keys = dict(
+	consumer_key =          'placeholder',
+	consumer_secret =       'placeholder',
+	access_token =          'placeholder-placeholder',
+	access_token_secret =   'placeholder'
+)
